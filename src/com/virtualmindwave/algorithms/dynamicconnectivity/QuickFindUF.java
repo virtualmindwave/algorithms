849f2ba5a010ec831bff002f2d594e30669bb909
virtualmindwave <virtualmindwave@gmail.com>
2016-11-19 21:19:44 +0800

    finalized comments for QuickFind algorithm and test class

--- src/com/virtualmindwave/algorithms/dynamicconnectivity/QuickFindUF.java
+++ src/com/virtualmindwave/algorithms/dynamicconnectivity/QuickFindUF.java
@@ -3,17 +3,23 @@ package com.virtualmindwave.algorithms.dynamicconnectivity;
  *  Author:        virtualmindwave
  *  Written:       11/19/2016
  *
- *  This is my implementation of the Quick Find
- *  Algorithm for Dynamic Connectivity.
+ *  Implementation of the Quick Find Algorithm 
+ *  for Dynamic Connectivity.
  *  
+ *  usage: Generate array of integers
+ *  n = 10
+ *  [0][1][2][3][4][5][6][7][8][9]
  *  
+ *  union(1, 5) - this will link 1 and 5
+ *  [0][5][2][3][4][5][6][7][8][9]
+ *  connected(1, 5) - this will check if 1 and 5 is connected
  *  
  *----------------------------------------------------------------*/
 public class QuickFindUF {
     private int[] arr;
     
     /**
-     *  Constructor that initializes array values
+     *  Constructor that initializes sample array values n
      */
     public QuickFindUF(int n){
         arr = new int[n];
@@ -24,8 +30,11 @@ public class QuickFindUF {
     }
 
     /**
-     *   
-     *   
+     *  To link the arrays method will need to assign the element   
+     *  arr[x] to arr[y] everytime.
+     *  This will also check every element and will assign to 
+     *  all other element linked before.
+     *  
      */
     public void union(int x, int y){
         int oldId = arr[x];
@@ -39,6 +48,9 @@ public class QuickFindUF {
 
     }
     
+    /**
+     *  Will check if elements are connected
+     */
     public boolean connected(int x, int y){
         if(arr[x] == arr[y]){
             return true;
@@ -46,6 +58,9 @@ public class QuickFindUF {
         return false;
     }
 
+    /**
+     *  Sample print output to check elements
+     */
     public void print(){
         for (int i=0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
--- src/com/virtualmindwave/algorithms/dynamicconnectivity/QuickFindUFMain.java
+++ src/com/virtualmindwave/algorithms/dynamicconnectivity/QuickFindUFMain.java
@@ -1,5 +1,19 @@
 package com.virtualmindwave.algorithms.dynamicconnectivity;
-
+/*----------------------------------------------------------------
+ *  Author:        virtualmindwave
+ *  Written:       11/19/2016
+ *
+ *  Test method for QuickFindUF algorithm
+ *  
+ *  usage: Generate array of integers
+ *  n = 10
+ *  [0][1][2][3][4][5][6][7][8][9]
+ *  
+ *  union(1, 5) - this will link 1 and 5
+ *  [0][5][2][3][4][5][6][7][8][9]
+ *  connected(1, 5) - this will check if 1 and 5 is connected
+ *  
+ *----------------------------------------------------------------*/
 class QuickFindUFMain {
     public static void main(String args[]){
         QuickFindUF qf = new QuickFindUF(10);
